ca0f9a14d2aec649592fd4c2e9c72edf7b074b1a
Slice <sergey.slice@gmail.com>
2025-02-05 19:18:45 +0300

    new patches
    Signed-off-by: Slice <sergey.slice@gmail.com>

---
+++ WineStagingPats/WineStagingPats/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  WineStagingPats
+//
+//  Created by Sergey on 28/07/2021.
+//  Copyright © 2021 Sergey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
